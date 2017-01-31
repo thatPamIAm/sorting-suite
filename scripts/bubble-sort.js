955a8a81e7d50ae1ed4f97aa111a7cf80511f865
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-01-31 15:17:51 -0700

    initial commit

---
+++ scripts/bubble-sort.js
@@ -0,0 +1,19 @@
+const bubbleSort = ()=> {
+
+}
+
+
+// const bubblefilterMe = (array, string) => {
+//
+//   validateString(string);
+//
+//   let filtered = array.filter((value) => {
+//     return value !== string
+//   });
+//
+//   return filtered;
+// }
+//
+// export default filterMe
+
+export default bubbleSort
